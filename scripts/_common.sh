b4b5aa550973a8713400122dc1cba63aeddec442
rosbeef andino <romain.raynaud@3cmr.fr>
2022-04-13 19:18:10 -0400

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="openjdk-8-jre-headless|openjdk-11-jre-headless debconf|debconf-2.0 procps uuid-runtime lua-ldap"
+pkg_dependencies_arm="automake autoconf build-essential libtool git maven m4"
 pkg_extra_dependencies_arm="openjdk-8-jre_8u322-b06-1~deb9u1_armhf.deb \
                             openjdk-8-jre-headless_8u322-b06-1~deb9u1_armhf.deb \
                             openjdk-8-jdf_8u322-b06-1~deb9u1_armhf.deb \
