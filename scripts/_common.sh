02313631fc53f83cfa7088274cc3086d60a1108f
rosbeef andino <romain.raynaud@3cmr.fr>
2022-04-13 18:50:49 -0400

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,7 @@
 
 # dependencies used by the app
 pkg_dependencies="openjdk-8-jre-headless|openjdk-11-jre-headless debconf|debconf-2.0 procps uuid-runtime lua-ldap"
+pkg_extra_dependencies_arm="openjdk-8-jre_*_armhf.deb openjdk-8-jre-headless_*_armhf.deb openjdk-8-jdf_*_armhf.deb openjdk-8-jdk-headless_*_armhf.deb"
 
 #=================================================
 # PERSONAL HELPERS
