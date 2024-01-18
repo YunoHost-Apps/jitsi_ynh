1e3108a85ae3560ebf0422114bd59c1e7afed899
Félix Piédallu <felix@piedallu.me>
2024-01-18 16:39:14 +0100

    Update manifestv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,10 +8,29 @@
 # PERSONAL HELPERS
 #=================================================
 
-ynh_version_gt ()
-{
-    dpkg --compare-versions "$1" gt "$2"
+_setup_sources() {
+    # Download, check integrity, uncompress and patch the source from app.src
+    declare -A packages=(
+        [jitsi-jicofo]="jicofo"
+        [jitsi-meet-prosody]="jitsi-meet/prosody-plugins"
+        [jitsi-meet-web]="jitsi-meet"
+        [jitsi-videobridge]="jitsi-videobridge"
+    )
+
+    for package in "${!packages[@]}"; do
+        ynh_setup_source --dest_dir="$install_dir/temp" --source_id="$package"
+        pushd "$install_dir/temp"
+            ar x "$package.deb" data.tar.xz
+            tar xf data.tar.xz
+        popd
+
+        mv "$install_dir/temp/usr/share/${packages[$package]}/" "$install_dir/$package/"
+        ynh_secure_remove --file="$install_dir/temp"
+    done
+
+    ynh_setup_source --dest_dir="$install_dir/jitsi-meet-prosody" --source_id=mod_auth_ldap
 }
+
 
 ynh_jniwrapper_armhf ()
 {
@@ -27,20 +46,18 @@ ynh_jniwrapper_armhf ()
     packages_arm[jitsi-sctp]="jitsi-sctp"
     packages_arm[usrsctp]="jitsi-sctp/usrsctp/usrsctp"
 
-    for package_arm in "${!packages_arm[@]}"
-    do
-      ynh_setup_source --dest_dir="$tempdir/${packages_arm[$package_arm]}" --source_id=$package_arm
+    for package_arm in "${!packages_arm[@]}"; do
+        ynh_setup_source --dest_dir="$tempdir/${packages_arm[$package_arm]}" --source_id=$package_arm
     done
 
     # needed to make compile works
-    if [ ! -d "$tempdir/jitsi-sctp/jniwrapper/native/src/main/resources/lib/linux-arm/" ]
-    then
-      mkdir -p $tempdir/jitsi-sctp/jniwrapper/native/src/main/resources/lib/linux-arm/
+    if [ ! -d "$tempdir/jitsi-sctp/jniwrapper/native/src/main/resources/lib/linux-arm/" ]; then
+        mkdir -p $tempdir/jitsi-sctp/jniwrapper/native/src/main/resources/lib/linux-arm/
     fi
 
     pushd "$tempdir/jitsi-sctp"
-      mvn package -DbuildSctp -DbuildNativeWrapper -DdeployNewJnilib -DskipTests
-      mvn package
+        mvn package -DbuildSctp -DbuildNativeWrapper -DdeployNewJnilib -DskipTests
+        mvn package
     popd
 
     # rm official jniwrapper to copy
